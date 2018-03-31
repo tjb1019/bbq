2f5d8f87f47db619691aaa79c9630fe2d26ce57e
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2018-03-30 22:28:32 -0700

    Add catch all to server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 //Install express server
+const path = require ('path');
 const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
