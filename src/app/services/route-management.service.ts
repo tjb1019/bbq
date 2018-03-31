a499eec6509cb2e3937388b486f00ef6100bfad6
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2018-03-31 11:08:07 -0700

    Compressed images.  Fixed active nav item issue.

--- src/app/services/route-management.service.ts
+++ src/app/services/route-management.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import { ROUTES } from '../utilities/constants';
 import { Route } from '../models/route';
 
 @Injectable()
 export class RouteManagementService {
 
   activeRoute: Route;
-  
-  constructor() { }
+
+  constructor() {
+    switch (window.location.pathname) {
+      case ('/menu'):
+        this.activeRoute = ROUTES[0];
+        break;
+      case ('/contact'):
+        this.activeRoute = ROUTES[1];
+        break;
+      case ('/about'):
+        this.activeRoute = ROUTES[2];
+        break;
+      case ('/photos'):
+        this.activeRoute = ROUTES[3];
+        break;
+    }
+  }
 
   activateRoute(route: Route): void {
     this.activeRoute = route;
